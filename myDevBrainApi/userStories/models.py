6e89f886aba4cf892934bd96921b819a270fe677
Charlie Allen <runfast2.718r@gmail.com>
2022-04-16 17:15:17 -0500

    adding user story creation and display

--- myDevBrainApi/userStories/models.py
+++ myDevBrainApi/userStories/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Status(models.Model):
+class UserStory(models.Model):
     IDEATED = 'ID'
     PLANNED = 'P'
     INPROGRESS = 'IP'
@@ -16,22 +16,38 @@ class Status(models.Model):
         ('IT', 'In Test'),
         ('D', 'Done'),
     ]
+    # Update form fields for story creation as fields are added
+    FORM_FIELDS = [
+        {
+            "field": 'short_name',
+            "display": "User story name"
+        },
+        {
+            "field": 'full_name',
+            "display": "User story long form name"
+        },
+        {
+            "field": 'parent_feature',
+            "display": "Parent Feature"
+        },
+        {
+            "field": 'git_branch',
+            "display": "Git Branch Name"
+        },
+    ]
+
+    short_name = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=500)
     status = models.CharField(
         max_length=2,
         choices=STATUS_CHOICES,
         default=IDEATED,
     )
+    parent_feature = models.CharField(max_length=50)
+    git_branch = models.CharField(max_length=50)
 
     def is_completed(self):
         return self.status in {self.DONE}
-
-
-class UserStory(models.Model):
-    short_name = models.CharField(max_length=100)
-    full_name = models.CharField(max_length=500)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE)
-    parent_feature = models.CharField(max_length=50)
-    git_branch = models.CharField(max_length=50)
 
 
 class Comment(models.Model):
